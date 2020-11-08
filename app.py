3e068a2c57f29ed62a05ed7174f52bc3b5000bb7
krishnashenoy123 <krishnashenoy123.com>
2020-11-08 17:03:51 +0530

    did

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/Hi page')
+@app.route('/')
 def index():
     return render_template('index.html')
 
+
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
     app.run(debug = True)
 
